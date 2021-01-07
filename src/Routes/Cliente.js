690902be44b31b0f399dfdb19cbb368220cfb0cb
Militao36 <matheusmoreira3693@gmail.com.br>
2021-01-07 14:40:24 -0300

    Adicionado routes

--- src/Routes/Cliente.js
+++ src/Routes/Cliente.js
@@ -4,6 +4,11 @@ import ClienteController from '../Controllers/Cliente.controller';
 const router = Router();
 const cliente = new ClienteController();
 
+router.get('/', (req, res) => {
+    return res.render('cliente', {
+        title: 'BMS Optica'
+    })
+})
 router.post('/', cliente.post);
 router.put('/:id', cliente.put);
 router.delete('/:id', cliente.delete);
--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -3,7 +3,9 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-  return res.render('cliente')
+  return res.render('home', {
+    title: 'BMS Optica'
+  })
 });
 
 export default router;
