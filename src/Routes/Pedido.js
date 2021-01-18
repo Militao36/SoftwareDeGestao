8719e0f1be50cd8342fdc0d5c4e7f64a4977159c
Militao36 <matheusmoreira3693@gmail.com.br>
2021-01-18 14:49:32 -0300

    Adicionado o attr data-comissao

--- src/Routes/Pedido.js
+++ src/Routes/Pedido.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import PedidoController from '../Controllers/Pedido.controller';
 import ClienteRepo from '../Repositories/Clientes';
 import StatusRepo from '../Repositories/StatusPedido';
+import FuncionarioRepo from '../Repositories/Funcionario'
 
 
 const router = Router();
@@ -14,9 +15,11 @@ router.get('/', async (req, res) => {
     const status = await StatusRepo.buscarFiltro(`
         select uuid,nomeStatus from statusPedido where idEmpresa = '${req.idEmpresa}'`
     )
-    const funcionario = await StatusRepo.buscarFiltro(`
-        select uuid,nome from funcionario where idEmpresa = '${req.idEmpresa}'`
+    const funcionario = await FuncionarioRepo.buscarFiltro(`
+        select uuid,nome,comissao from funcionario where idEmpresa = '${req.idEmpresa}'`
     )
+
+    console.log(funcionario[0])
     return res.render('pedido', {
         title: 'BMS Optica',
         cliente: clientes[0],
